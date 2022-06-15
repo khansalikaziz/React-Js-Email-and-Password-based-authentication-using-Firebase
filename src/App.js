2d3a8417c88f8b658fffa29b0ac97735dac2ddbc
salik78 <77449919+salik78@users.noreply.github.com>
2022-06-15 13:12:26 +0530

    Authentication and Database

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import Firebase from "./Firebase";
+import { getDatabase, ref, set  } from "firebase/database";
+import NavBar from "./NavBar";
+
+
 
 function App() {
+ const firebaseConfig = {
+  apiKey: "AIzaSyAmVIkak1UMqSvZZCmoOGbBjg881kvbf94",
+  authDomain: "authenticationwithreact-e8001.firebaseapp.com",
+  databaseURL: "https://authenticationwithreact-e8001-default-rtdb.firebaseio.com",
+  projectId: "authenticationwithreact-e8001",
+  storageBucket: "authenticationwithreact-e8001.appspot.com",
+  messagingSenderId: "132631946667",
+  appId: "1:132631946667:web:f045d6dc4d4715cee01eee",
+  measurementId: "G-8F1FHE7HYG"
+  };
+  const app = initializeApp(firebaseConfig);
+  const database = getDatabase(app);
+  const analytics = getAnalytics(app);
+  const auth = getAuth();
+  function signin(){
+    
+      var email = document.getElementById("email").value;;
+  
+      
+      var password=document.getElementById("inputPassword").value;
+    
+      console.log(email)
+      console.log(password)
+      createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user+" signin successfull")
+        let userId=user.uid;
+        function writeUserData(userId,email,password) {
+          const db = getDatabase();
+          set(ref(db, 'users/' + userId), {
+            email: email,
+            password: password,
+            
+          });
+        }
+        writeUserData(userId,email,password)
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode+" :"+errorMessage )
+      });
+  
+    
+    
+    
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <NavBar/>
+    <div className="container my">
+      
+      <h3 className="heading my-5">Email and password based authenticcation</h3>
+    <input id="email" placeholder="Enter your Email" type="text" className="form-control my-3" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" style={{width:'400px'}}></input>
+     <input type="password" placeholder="Enter your password" className="form-control my-3" id="inputPassword" style={{width:'400px'}}></input>
+     <button type="button" className="btn btn-primary my-3" onClick={signin}>Submit</button>
+
     </div>
+         
+    </>
+    
   );
 }
 
